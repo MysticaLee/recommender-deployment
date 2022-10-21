18411c979e2720c4e5b327f64154fae257b8ebdd
JL <jasminelxy94@gmail.com>
2022-10-22 01:16:08 +0800

    Change port to default port

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,5 +5,5 @@ threads = 2
 workers = 2
 timeout = 0
 host = "0.0.0.0"
-bind = f":{os.environ.get('PORT', '3000')}"
+bind = f":{os.environ.get('PORT', '10000')}"
 worker_class = "uvicorn.workers.UvicornWorker"
